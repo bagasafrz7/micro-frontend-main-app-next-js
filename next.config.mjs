b8d0059396c3d92a522896324cd9cab63384f37b
bagasafrz7 <bagasafrz16@gmail.com>
2024-07-31 11:32:53 +0700

    feat: setup module-federation and remote project shop_app

---
+++ next.config.mjs
@@ -0,0 +1,29 @@
+import NextFederationPlugin from '@module-federation/nextjs-mf';
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  webpack(config, options) {
+    const { isServer } = options;
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: 'main_app',
+        remotes: {
+          shop_app: `shop_app@http://localhost:3001/_next/static/${
+            isServer ? 'ssr' : 'chunks'
+          }/remoteEntry.js`,
+        },
+        filename: 'static/chunks/remoteEntry.js',
+        exposes: {},
+        extraOptions: {
+          debug: false, // `false` by default
+          exposePages: false, // `false` by default
+        },
+        shared: {},
+      })
+    );
+    return config;
+  },
+};
+
+export default nextConfig;
